7c297e2d5e31cd087fdb98e46f4e237c262f1955
CRFitzgerald <117607189+CRFitzgerald@users.noreply.github.com>
2023-02-25 19:52:24 -0800

    analysis

---
+++ EmployeeSQL/data-analysis.sql
@@ -0,0 +1,57 @@
+--List the employee number, last name, first name, sex, and salary of each employee
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+INNER JOIN salaries ON
+salaries.emp_no=employees.emp_no
+;
+
+--List the first name, last name, and hire date for the employees who were hired in 1986
+SELECT first_name, last_name, hire_date
+FROM employees 
+WHERE hire_date between '1985-12-31' and '1987-01-01'
+;
+
+--List the manager of each department along with their department number, department name, employee number, last name, and first name
+SELECT dept_manager.emp_no, departments.dept_no, departments.dept_name, employees.last_name, employees.first_name
+FROM departments
+LEFT JOIN dept_manager ON dept_manager.dept_no = departments.dept_no
+LEFT JOIN  employees ON dept_manager.emp_no = employees.emp_no
+ORDER BY dept_manager.emp_no
+;
+
+--List the department number for each employee along with that employee’s employee number, last name, first name, and department name
+SELECT dept_emp.emp_no, dept_emp.dept_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+RIGHT JOIN employees ON employees.emp_no = dept_emp.emp_no
+LEFT JOIN departments ON departments.dept_no = dept_emp.dept_no
+ORDER BY dept_emp.emp_no 
+;
+
+--List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' and last_name LIKE 'B%'
+;
+
+--List each employee in the Sales department, including their employee number, last name, and first name
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name
+FROM departments
+LEFT JOIN dept_emp ON dept_emp.dept_no = departments.dept_no
+LEFT JOIN  employees ON employees.emp_no = dept_emp.emp_no
+WHERE departments.dept_name = 'Sales'
+;
+--List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name
+SELECT departments.dept_name, dept_emp.emp_no, employees.last_name, employees.first_name
+FROM departments
+LEFT JOIN dept_emp ON dept_emp.dept_no = departments.dept_no
+LEFT JOIN  employees ON dept_emp.emp_no = employees.emp_no
+WHERE departments.dept_name = 'Sales' or departments.dept_name = 'Development'
+ORDER BY departments.dept_name
+;
+
+--List the frequency counts, in descending order, of all the employee last names
+SELECT last_name, COUNT(last_name) AS "surname count"
+FROM employees
+GROUP BY last_name
+ORDER BY "surname count" DESC;
+;
